006a1704cc7b9dc80ae5d2b707ce19bedbf75528
liuyuliang <gugnv@126.com>
2023-09-16 16:32:55 +0800

    add file download  demo

---
+++ j-file-example/src/main/java/org/example/file/FileApplication.java
@@ -0,0 +1,18 @@
+package org.example.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: FileApplication
+ * Description:
+ *
+ * @author Yuliang Lau
+ * @date 2023/9/16 16:18
+ */
+@SpringBootApplication
+public class FileApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FileApplication.class, args);
+    }
+}
---
+++ j-file-example/src/main/java/org/example/file/controller/FileController.java
@@ -0,0 +1,50 @@
+package org.example.file.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
+
+/**
+ * ClassName: FileController
+ * Description:
+ *
+ * @author Yuliang Lau
+ * @date 2023/9/16 16:15
+ */
+@RestController
+@RequestMapping("/file")
+public class FileController {
+    /**
+     * 指想要下载的文件的路径
+     *
+     * @param response
+     * @功能描述 下载文件:将输入流中的数据循环写入到响应输出流中，而不是一次性读取到内存
+     */
+    @GetMapping("/downloadLocal")
+    public void downloadLocal(HttpServletResponse response) throws IOException {
+        String path = "F:\\project\\IDEA_2023\\store\\linjiashop\\linjiashop-admin-api\\src\\main\\resources\\templates\\orderList.xlsx";
+        // 读到流中
+        InputStream inputStream = new FileInputStream(path);// 文件的存放路径
+        response.reset();
+        response.setContentType("application/octet-stream");
+        String filename = new File(path).getName();
+        response.addHeader("Content-Disposition", "attachment; filename=" + URLEncoder.encode(filename, "UTF-8"));
+        ServletOutputStream outputStream = response.getOutputStream();
+        byte[] b = new byte[1024];
+        int len;
+        //从输入流中读取一定数量的字节，并将其存储在缓冲区字节数组中，读到末尾返回-1
+        while ((len = inputStream.read(b)) > 0) {
+            outputStream.write(b, 0, len);
+        }
+        inputStream.close();
+    }
+
+}
