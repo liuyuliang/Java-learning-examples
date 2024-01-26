addd53e48daa5dba1dea7f18ae65719766ab41f7
liuyuliang <gugnv@126.com>
2024-01-26 09:54:04 +0800

    nacos

--- j-nacos-example/src/main/java/org/example/nacos/NacosApplication.java
+++ j-nacos-example/src/main/java/org/example/nacos/NacosApplication.java
@@ -1,9 +1,18 @@
-package org.example.nacos;/**
- *
+package org.example.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
  * ClassName: NacosApplication
  * Description:
+ *
  * @author Yuliang Lau
  * @date 2024/1/25 15:29
  */
+@SpringBootApplication
 public class NacosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosApplication.class,args);
+    }
 }
