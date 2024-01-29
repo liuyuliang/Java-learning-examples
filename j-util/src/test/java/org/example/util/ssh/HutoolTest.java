0e53b1ee66730cfb2ccdbc4d1ae26a0b114b2439
liuyuliang <gugnv@126.com>
2024-01-29 17:09:37 +0800

    job

--- j-util/src/test/java/org/example/util/ssh/HutoolTest.java
+++ j-util/src/test/java/org/example/util/ssh/HutoolTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * ClassName: HutoolTest
@@ -15,7 +18,7 @@ import java.time.format.DateTimeFormatter;
  */
 public class HutoolTest {
     @Test
-    public void testDate(){
+    public void testDate() {
         // 获取当前日期
         LocalDate today = LocalDate.now();
 
@@ -30,5 +33,39 @@ public class HutoolTest {
 
         // 输出结果
         System.out.println("前两个月的日期（格式为 yyyyMM）是: " + formattedDate);
+    }
+
+    @Test
+    public void testMonth() {
+        String startDate = "202201"+"01"; // 开始日期，格式为yyyyMM
+        String endDate = "202305"+"01"; // 结束日期，格式为yyyyMM
+
+        // 将字符串转换为LocalDate对象
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        LocalDate start = LocalDate.parse(startDate, formatter);
+        LocalDate end = LocalDate.parse(endDate, formatter);
+
+        // 计算两个日期之间的所有日期
+        List<String> dates = getDates(start, end);
+
+        // 输出结果
+        for (String date : dates) {
+            System.out.println(date);
+        }
+    }
+
+
+
+
+    /**
+     * 获取两个日期之间的所有日期
+     */
+    private static List<String> getDates(LocalDate start, LocalDate end) {
+        List<String> dates = new ArrayList<>();
+        while (!start.isAfter(end)) {
+            dates.add(start.format(DateTimeFormatter.ofPattern("yyyyMM")));
+            start = start.plus(1, ChronoUnit.MONTHS);
+        }
+        return dates;
     }
 }
--- j-xxl-job-example/src/main/java/org/example/xxl/job/task/Job.java
+++ j-xxl-job-example/src/main/java/org/example/xxl/job/task/Job.java
@@ -1,6 +1,10 @@
 package org.example.xxl.job.task;
 
+import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 /**
  * ClassName: Job
@@ -9,9 +13,18 @@ import com.xxl.job.core.handler.annotation.XxlJob;
  * @author Yuliang Lau
  * @date 2023/12/14 6:47
  */
+@Component
 public class Job {
     @XxlJob("demoJobHandler")
-    public void demoJobHandler(String param) throws Exception {
+    public void demoJobHandler() throws Exception {
         System.out.println("start job...................");
+    }
+    @XxlJob("demoDateJobHandler")
+    public void genMonth(String start,String end ){
+        // 获取参数
+        String param = XxlJobHelper.getJobParam();
+        String[] methodParams = param.split(",");
+        Arrays.stream(methodParams).forEach(System.out::println);
+
     }
 }
